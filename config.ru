7416dadd914a7919f936a1e04cff8db46dbf0cf4
Marciel <marciel@atmatecnologia.com.br>
2016-05-30 09:31:17 -0300

    separação de modulos

--- config.ru
+++ config.ru
@@ -7,12 +7,13 @@ require 'eventmachine'
 require './logging.rb'
 require './server.rb'
 require './api_module/analogic_process.rb'
+Dir.glob('./model/*.rb') { |file| load file }
 
 $path = File.dirname(File.expand_path(__FILE__))
 
-ip = '45.55.233.137'
+#ip = '45.55.233.137'
 
-#ip = '192.168.0.11'
+ip = '192.168.0.11'
 porta = 5580
 
 @pasta_pids = "#{$path}/tmp/pids"
---
+++ model/hora.rb
@@ -0,0 +1,14 @@
+class Hora
+  def self.gerar_atualizacao_hora
+    response = ''
+    data = Time.now.strftime("%y%m%d%H%M%S")
+
+    for i in 0..5
+      temp = data[2 * i ... (2 * i) + 2].to_i
+      response += temp.to_s(16).rjust(2, '0').upcase
+    end
+    logger.info "Pacote de atualização de Hora ---> #{response}"
+
+    "<00#{gerar_check_sum(response)}>"
+  end
+end
