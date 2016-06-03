c83c9ad887a69e1e1d2e9b1278249da69e387dae
Johnny Padilha <johnny.padilha1@gmail.com>
2016-06-03 13:52:49 -0300

    todas as posições do pacote 03

--- api_module/pacotes.rb
+++ api_module/pacotes.rb
@@ -26,9 +26,62 @@ class Pacotes
         timers_analogicas = pacote[92..123]
         timers_negativas = pacote[124..131]
         timers_digitais = pacote[132..140]
+        timer_periodico = pacote[148..151]
 
-        # medidas_brutas = pacote[10..21]
-        Pacotes::configuracao(analogicas_brutas, negativas_brutas, digitais_brutas, timers_analogicas, timers_negativas, timers_digitais)
+        operadora = pacote[152..153]
+
+        ip1_1octeto = pacote[154..155]
+        ip1_2octeto = pacote[156..157]
+        ip1_3octeto = pacote[158..159]
+        ip1_4octeto = pacote[160..161]
+        porta_ip1 = pacote[162..165]
+
+        ip2_1octeto = pacote[166..167]
+        ip2_2octeto = pacote[168..169]
+        ip2_3octeto = pacote[170..171]
+        ip2_4octeto = pacote[172..173]
+        porta_ip2 = pacote[174..177]
+        host = pacote[178..183]
+        porta_dns = pacote[184..187]
+
+        timer_periodico = timer_periodico.hex
+
+        ip1_1octeto = ip1_1octeto.hex
+        ip1_2octeto = ip1_2octeto.hex
+        ip1_3octeto = ip1_3octeto.hex
+        ip1_4octeto = ip1_4octeto.hex
+        porta_ip1 = porta_ip1.hex
+
+        ip2_1octeto = ip1_1octeto.hex
+        ip2_2octeto = ip1_2octeto.hex
+        ip2_3octeto = ip1_3octeto.hex
+        ip2_4octeto = ip1_4octeto.hex
+        porta_ip2 = porta_ip1.hex
+
+        operadora = operadora.hex
+        case operadora.to_i
+        when 1
+          p operadora = "TIM"
+        when 2
+          p operadora = "VIVO M2M / SMARTCENTER"
+        when 3
+          p operadora = "BRASIL TELECOM"
+        when 4
+          p operadora = "VIVO"
+        when 5
+          p operadora = "OI"
+        else
+          p operadora
+        end
+
+        porta_dns = porta_dns.hex
+        if host.hex == 0
+          host = host.hex
+        else
+          host = host.split.pack('H*').gsub("\0","")
+        end
+
+        Pacotes::configuracao(analogicas_brutas, negativas_brutas, digitais_brutas, timers_analogicas, timers_negativas, timers_digitais, timer_periodico)
         firmware = ProcessarPacotes::obtem_firmware pacote
     when 4
       logger.info "\n"
@@ -72,13 +125,7 @@ class Pacotes
     pacote = pacote.tr!('>', '')
   end
 
-  def self.configuracao(analogicas_brutas, negativas_brutas, digitais_brutas, timers_analogicas, timers_negativas, timers_digitais)
-    # p "analógicas brutas: #{analogicas_brutas}"
-    # p "\n NEGATIVAS brutas: #{negativas_brutas}"
-    # p "\n TIMERs brutOs: #{timers_brutos.size}"
-    # p "\n TIMERs ana: #{timers_analogicas}"
-    p "\n TIMERs neg: #{timers_negativas}"
-    # p "\n TIMERs digi: #{timers_digitais}"
+  def self.configuracao(analogicas_brutas, negativas_brutas, digitais_brutas, timers_analogicas, timers_negativas, timers_digitais, timer_periodico)
 
     digitais_bin = digitais_brutas.hex.to_s(2)
 
@@ -86,19 +133,16 @@ class Pacotes
 
     digitais_bin = digitais_bin[0..3]
 
+    timer_periodico = timer_periodico.hex / 60
 
     cont = 0
     time_cont = 0
-    # medida = Array.new
     medidas = Hash.new
-    # p dig
 
     16.times do |i|
-      # medidas[:"A#{i+1}"] = analogicas_brutas[cont ... cont+4]
       medidas[:"A#{i+1}-min"] = analogicas_brutas[cont ... cont+2].to_i(16) * 100 / 255
       medidas[:"A#{i+1}-max"] = analogicas_brutas[cont+2 ... cont+4].to_i(16) * 100 / 255
       medidas[:"A#{i+1}-max"] = analogicas_brutas[cont+2 ... cont+4].to_i(16) * 100 / 255
-
       medidas[:"A#{i+1}-timer"] = timers_analogicas[time_cont ... time_cont+2].hex.to_s(10)
 
       time_cont = time_cont+2
@@ -109,47 +153,25 @@ class Pacotes
     time_cont = 0
 
     4.times do |i|
-      # medidas[:"N#{i+1}"] = negativas_brutas[cont ... cont+4]
       medidas[:"N#{i+1}-min"] = negativas_brutas[cont ... cont+2].to_i(16) * 100 / 255
       medidas[:"N#{i+1}-max"] = negativas_brutas[cont+2 ... cont+4].to_i(16) * 100 / 255
-      medidas[:"N#{i+1}-timer"] = timers_negativas[time_cont ... time_cont+2]#.hex.to_s(10)
+      medidas[:"N#{i+1}-timer"] = timers_negativas[time_cont ... time_cont+2].hex.to_s(10)
       time_cont = time_cont+2
       cont = cont+4
     end
 
-    # 20.times do |i|
-    #   medida.push(med[cont ... cont+4])
-    #   cont = cont+4
-    # end
-
     time_cont = 0
 
     4.times do |i|
-      # medidas[:"D#{i+1}"] = digitais_bin
       medidas[:"D#{i+1}-normal"] = digitais_bin[i-1]
       medidas[:"D#{i+1}-timer"] = timers_digitais[time_cont ... time_cont+2].hex.to_s(10)
       time_cont = time_cont+2
     end
 
-    # 24.times do |i|
-    #   medida[i-1].push('t')
-    # end
-
      medidas.each do |k,v|
-    #    logger.info "#{k} => #{v}"
-       p "#{k} => #{v}"
-    #    logger.info "-----------------"
+       #logger.info "#{k} => #{v}"
+       #p "#{k} => #{v}"
      end
 
-    # min = med[cont,2]
-    # max = med[cont+2,2]
-    # timer = med[cont+2,2]
-    # p "*"*5
-    # p "min: #{min}"
-    # p "*"*5
-    # p "max: #{max}"
-    # p "*"*5
-    # p "timer: #{timer}"
-    p "*"*5
   end
 end
