b1f0733874c2c1b92bd29d009f9f0e0722dc1f8f
Marciel <marciel@atmatecnologia.com.br>
2016-08-31 14:38:32 -0300

    debug for reporte

--- model/evento.rb
+++ model/evento.rb
@@ -42,7 +42,7 @@ class Evento < ActiveRecord::Base
             else
               SelecionarPacote.new({codigo_atual: codigo_evento, codigo_pacote: evento[:codigo_pacote], status_faixa: status_faixa}).seleciona_pacote
             end
-          if status_faixa.to_i == ALERTA or status_faixa.to_i == ALARME
+          if status_faixa.to_i == ALERTA || status_faixa.to_i == ALARME
             reporte_faixa = true if medida.reporte_medida_id == REPORTE_FAIXA
             reporte_sinal = true if medida.reporte_medida_id == REPORTE_SINAL
             reporte_energia = true if medida.reporte_medida_id == REPORTE_ENERGIA
@@ -50,6 +50,7 @@ class Evento < ActiveRecord::Base
           end
           Logging.info "DEBUG ---------- REPORT-----"
           Logging.info faixa_atual.inspect
+          Logging.info medida.timer
           Logging.info status_faixa
           Logging.info reporte_faixa
           Logging.info reporte_sinal
