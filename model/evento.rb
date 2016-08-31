695d7810451fc22d91faaabba40ff8c3d631602e
Marciel <marciel@atmatecnologia.com.br>
2016-08-31 14:22:36 -0300

    debug for reporte

--- model/evento.rb
+++ model/evento.rb
@@ -49,7 +49,7 @@ class Evento < ActiveRecord::Base
             reporte_temperatura = true if medida.reporte_medida_id == REPORTE_TEMPERATURA
           end
           Logging.info "DEBUG ---------- REPORT-----"
-          Logging.info faixa_atual
+          Logging.info faixa_atual.inspect
           Logging.info status_faixa
           Logging.info reporte_faixa
           Logging.info reporte_sinal
