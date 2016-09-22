5358fe6dbcb87d747756594a59c78837cb0b20de
Marciel <marciel@atmatecnologia.com.br>
2016-09-22 09:08:27 -0300

    reset no metodo processar_pacotes do gerentew

--- model/saida.rb
+++ model/saida.rb
@@ -2,15 +2,6 @@ class Saida < ActiveRecord::Base
   self.table_name = "main.saidas"
 
   def self.check_out cancelado, data_processamento, modelo_id, aguardando
-    saidas = Saida.where('cancelado = ? and data_processamento is ? and modelo_id = ? and aguardando = ?', false, nil, 1, false).pluck(:telemetria_id)
-
-    saidas = saidas.uniq
-
-    total_saidas = []
-    saidas.each do |saida|
-      total_saidas << Saida.where('cancelado = ? and data_processamento is ? and modelo_id = ? and aguardando = ? and telemetria_id = ?', false, nil, 1, false, saida).first
-    end
-
-    total_saidas.compact
+    Saida.where('cancelado = ? and data_processamento is ? and modelo_id = ? and aguardando = ?', false, nil, 1, false).first
   end
 end
