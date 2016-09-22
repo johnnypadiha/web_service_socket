dfc5dd65db57e2580f58ac1ceb63bf2e41e198aa
Marciel <marciel@atmatecnologia.com.br>
2016-09-22 08:20:44 -0300

    adicionado timmer de 0.5 no metodo checar saida

--- api_module/gerente_module.rb
+++ api_module/gerente_module.rb
@@ -38,8 +38,11 @@ class GerenteModule < EventMachine::Connection
   #         telemetria
   #
   def self.checar_saida
-    saida = Saida.check_out false, nil, 1, false
-    GerenteModule.processar_comandos(saida) if saida
+    saidas = Saida.check_out false, nil, 1, false
+    saidas.each do |saida|
+      GerenteModule.processar_comandos(saida) if saida
+      sleep 0.5
+    end
   end
 
   # Internal : recebe um pedido de processamento de comando verifica se o mesmo
--- model/saida.rb
+++ model/saida.rb
@@ -2,6 +2,15 @@ class Saida < ActiveRecord::Base
   self.table_name = "main.saidas"
 
   def self.check_out cancelado, data_processamento, modelo_id, aguardando
-    Saida.where('cancelado = ? and data_processamento is ? and modelo_id = ? and aguardando = ?', false, nil, 1, false).first
+    saidas = Saida.where('cancelado = ? and data_processamento is ? and modelo_id = ? and aguardando = ?', false, nil, 1, false).pluck(:telemetria_id)
+
+    saidas = saidas.uniq
+
+    total_saidas = []
+    saidas.each do |saida|
+      total_saidas << Saida.where('cancelado = ? and data_processamento is ? and modelo_id = ? and aguardando = ? and telemetria_id = ?', false, nil, 1, false, saida).first
+    end
+
+    total_saidas.compact
   end
 end
