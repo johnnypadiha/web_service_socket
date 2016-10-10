c6ebed194771d566346ecaf030e7e81c7986feb4
Marciel <marciel@atmatecnologia.com.br>
2016-10-10 16:49:34 -0300

    log_id renomeado para log_comunicacao

--- api_module/analogic_process.rb
+++ api_module/analogic_process.rb
@@ -107,7 +107,7 @@ module AnalogicProcess
       else
         logger.info "pacote: #{data}, possui um formato inválido!".yellow
       end
-        logger.info "Telemetrias conectadas #{$lista_telemetria.select { |t| t[:id].to_i != 0}.size}".green
+        logger_comunicacao.info "Telemetrias conectadas #{$lista_telemetria.select { |t| t[:id].to_i != 0}.size}".green
     end
   end
 
