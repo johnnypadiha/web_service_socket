d81256ba1be44e5d8191b414378e0cba97544bba
Marciel <marciel@atmatecnologia.com.br>
2016-10-17 09:04:42 -0200

    fix mensagem de erro para uma telemetria não cadastrada

--- service/separar_medida_equipamento.rb
+++ service/separar_medida_equipamento.rb
@@ -19,27 +19,27 @@ class SepararMedidaEquipamento
     elsif telemetria.equipamentos.blank?
       logger.info 'Ainda não existem equipamentos associados a '\
                   "Telemetria #{medidas[:codigo_telemetria]}".yellow
-    end
+    else
+      telemetria.equipamentos.each do |equipamento|
+        eqm[:id_equipamento] = equipamento.id
+        referencia_medidas = equipamento.codigos || []
+        eqm[:codigo_telemetria] = medidas[:codigo_telemetria]
+        eqm[:DBM] = medidas[:DBM]
+        eqm[:codigo_pacote] = medidas[:tipo_pacote]
+        eqm[:telemetria_codigo] = telemetria.codigo
+        referencia_medidas.each do |cod|
+          eqm[cod.codigo.to_sym] = medidas[:leituras][cod.codigo.to_sym]
+        end
 
-    telemetria.equipamentos.each do |equipamento|
-      eqm[:id_equipamento] = equipamento.id
-      referencia_medidas = equipamento.codigos || []
-      eqm[:codigo_telemetria] = medidas[:codigo_telemetria]
-      eqm[:DBM] = medidas[:DBM]
-      eqm[:codigo_pacote] = medidas[:tipo_pacote]
-      eqm[:telemetria_codigo] = telemetria.codigo
-      referencia_medidas.each do |cod|
-        eqm[cod.codigo.to_sym] = medidas[:leituras][cod.codigo.to_sym]
+        if equipamento.codigos.present?
+          evento << eqm
+        else
+          logger.info "Um pacote foi recebido e ignorado, "\
+                       "Pois ainda não existem medidas associadas ao equipamento "\
+                       "Nome : #{equipamento.nome} / ID: #{equipamento.id}".yellow
+        end
+        eqm = {}
       end
-
-      if equipamento.codigos.present?
-        evento << eqm
-      else
-        logger.info "Um pacote foi recebido e ignorado, "\
-                     "Pois ainda não existem medidas associadas ao equipamento "\
-                     "Nome : #{equipamento.nome} / ID: #{equipamento.id}".yellow
-      end
-      eqm = {}
     end
     evento
   end
